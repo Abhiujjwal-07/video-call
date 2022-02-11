d5a1ed6ef454ea01ac7f2ad6763cb5d3ac95c24a
Abhiujjwal-07 <Abhiujjwal.pradhan@thewitslab.com>
2022-02-11 20:15:33 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("room", { roomId: req.params.room });
+});
+io.on("connection", (socket) => {
+    console.log("1");
+    socket.on("join-room", (roomId, userId) => {
+        console.log("2");
+        socket.join(roomId);
+        console.log("3");
+        socket.to(roomId).emit("user-connected", userId);
+        console.log(userId);
+        console.log(roomId);
+    });
+});
+server.listen(3400);
